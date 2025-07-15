3d4e3725de88f84b7271d8f72f83512db4f1d6fb
Adriano <adrianomarque135@gmail.com>
2025-07-14 23:44:49 -0300

    correção

--- bubble_sort.py
+++ bubble_sort.py
@@ -3,8 +3,8 @@ def bubble_sort(list):
     for i in range(len(list)):
 
         for l in range(len(list) - 1):
-            if list[l] > list[j+1]:
+            if list[l] > list[l+1]:
                 
-                list[j], list[j+1] = list[j+1], list[j]
+                list[l], list[l+1] = list[l+1], list[l]
 
     return list
